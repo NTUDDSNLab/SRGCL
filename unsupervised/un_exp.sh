5f957fbcccec7452a1a9c7be3a8bd2ee240341d4
gavinfu2 <81958446+gavinfu2@users.noreply.github.com>
2024-12-30 19:05:37 +0800

    Delete unsupervised directory

--- unsupervised/un_exp.sh
+++
@@ -1,6 +0,0 @@
-# datasets0=('MUTAG' 'PROTEINS' 'REDDIT-BINARY' 'DD' 'IMDB-BINARY' 'NCI1' 'COLLAB' 'REDDIT-MULTI-5K')
-datasets0=('MUTAG')
-for dataset in ${datasets0[*]}
-do
-    bash un_exp_rep.sh ${dataset} 0
-done
